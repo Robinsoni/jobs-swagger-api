5c158a4c3b69a8e0f3fb90e66ad6a35324715e38
Robin Soni <sonirobin369@gmail.com>
2023-10-02 22:11:02 +0530

    web security packages added.

--- app.js
+++ app.js
@@ -3,11 +3,30 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require("express");
 const app = express();
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+//
+
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 200, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 const jobsRouter =  require("./routes/jobs");
 const connectDB = require("./db/connect")
 const authRouter = require("./routes/auth");
 const authentication = require("./middleware/authentication");
+  
+
 app.use("/api/v1/auth",authRouter);
 app.use("/api/v1/jobs",authentication,jobsRouter);
 const port  = process.env.PORT || 5000; 
